4af6eb6caaa018a864cf2ffad1de4b6d3f9497da
Nitin Kumar <nknitinkumar12384@gmail.com>
2025-07-22 12:29:40 +0530

    Run test 10 times

--- rule_test.go
+++ rule_test.go
@@ -583,6 +583,8 @@ func runRuleListFiltered(t *testing.T, family int, srcNet, dstNet *net.IPNet) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			//rules_before, _ := RuleListFiltered(family, tt.ruleFilter, tt.filterMask)
+			//t.Logf("Got rules, before: %v", rules_before)
 			rule := tt.preRun()
 			wantRules, wantErr := tt.setupWant(rule)
 
